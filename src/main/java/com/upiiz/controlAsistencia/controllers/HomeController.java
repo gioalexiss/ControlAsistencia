d421af27abd726622be2661e2656bc64c6bebc7c
gioalexiss <aarg.gio.05@gmail.com>
2025-10-01 20:50:13 -0600

    Conexion entre login y listado

--- src/main/java/com/upiiz/controlAsistencia/controllers/HomeController.java
+++ src/main/java/com/upiiz/controlAsistencia/controllers/HomeController.java
@@ -2,11 +2,24 @@ package com.upiiz.controlAsistencia.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping
-    public String home() {
+    @GetMapping("/")
+    public String login() {
+        return "page-login"; // Aquí pones el nombre de tu archivo de login (sin .html)
+    }
+
+    @PostMapping("/login/valido")
+    public String loginValido() {
+        // Por ahora, redirige al listado sin validar
         return "index";
     }
+
+    @GetMapping("/listado")
+    public String listado() {
+        return "index"; // listado.html en templates
+    }
+
 }
